d7fa2cf8553763db3c838a860e8aa654cb353cf3
Josh Navarro <joshpatnav@gmail.com>
2026-07-29 13:14:57 -0700

    Need to finish the last file

--- 02 Grouping and Group Filters/02_grouping_group_filters.sql
+++ 02 Grouping and Group Filters/02_grouping_group_filters.sql
@@ -13,28 +13,208 @@ Goal: Learn how to group and use group filters
 
 -- 1 Group Rows by a Category
 
+-- The GROUP BY divides the rows into categories based on the
+--  unique values that are found in the column
+
+-- Show the appointment type, and the total appointments grouped by the department 
+
+SELECT
+    appointment_type,
+    COUNT(*) AS total_appointments
+FROM appointments 
+GROUP BY appointment_type;
+
+/*
+Output:
+appointment_type
+character varying
+total_appointments
+bigint
+1	Sick Visit	1
+2	Consultation	4
+3	Annual Exam	2
+4	Diagnostic Test	1
+5	Follow-Up	4
+*/
+
 -- 2 Grouping by Multiple Columns
+
+-- You are able to use the GROUP BY on more than one column, 
+-- For each column a group is created for each unique value
+
+-- Show the appointment type, status, and then the total appointments
+
+SELECT
+    appointment_type,
+    appointment_status,
+    COUNT(*) AS total_appointments
+FROM appointments 
+GROUP BY 
+    appointment_type,
+    appointment_status;
+
+/*
+Output:
+appointment_type
+character varying
+appointment_status
+character varying
+total_appointments
+bigint
+1	Follow-Up	Completed	2
+2	Consultation	Scheduled	1
+3	Consultation	Cancelled	2
+4	Sick Visit	Completed	1
+5	Follow-Up	No-Show	1
+6	Annual Exam	Completed	2
+7	Diagnostic Test	Completed	1
+8	Consultation	No-Show	1
+9	Follow-Up	Scheduled	1
+*/
 
 -- Summarizing Groups
 -- ==================================
 
 -- 3 Using Aggregate Function w/ Groups
 
--- 4 Using multiple Aggregate Functions
+-- Aggregate functions can be used to calculate 
+-- a seperate result for each of the groups.
+-- Such as: SUM(), COUNT(), MIN(), etc..
+
+-- show the appointment type, total appointments, total cost, and average cost 
+
+SELECT 
+    appointment_type,
+    COUNT(*) AS total_appointments,
+    ROUND(SUM(copay_amount), 2) AS total_paid,
+    ROUND(AVG(copay_amount), 2) AS average_appointment_cost
+FROM appointments
+GROUP BY appointment_type;
+
+/*
+Output:
+appointment_type
+character varying
+total_appointments
+bigint
+total_paid
+numeric
+average_appointment_cost
+numeric
+1	Sick Visit	1	15.00	15.00
+2	Consultation	4	50.00	25.00
+3	Annual Exam	2	60.00	30.00
+4	Diagnostic Test	1	50.00	50.00
+5	Follow-Up	4	105.00	26.25
+*/
 
 -- Filtering Grouped Analysis 
 -- ==================================
 
--- 5 Filtering rows with WHERE
+-- 4 Filtering rows with WHERE
 
--- 6 Filtering Groups with HAVING 
+-- You are able to filter the rows before grouping by, 
+-- only the rows that statisfy the condition will show
 
--- 7 Using WHERE and HAVING together
+-- Show the appointment type, the total for completed appointments
+SELECT
+    appointment_type,
+    COUNT(*) AS total_completed_appointments
+FROM appointments 
+WHERE appointment_status = 'Completed'
+GROUP BY appointment_type;
+
+/*
+Output:
+appointment_type
+character varying
+total_completed_appointments
+bigint
+1	Annual Exam	2
+2	Diagnostic Test	1
+3	Follow-Up	2
+4	Sick Visit	1
+*/
+
+-- 5 Filtering Groups with HAVING
+
+-- You can filter the groups after the aggregation by using the HAVING
+-- commonly used with COUNT(), SUM(), and AVG()
+
+-- Show the appointment type, the total appointments,
+-- and group by type, and those having more than 3
+
+SELECT
+    appointment_type,
+    COUNT(*) AS total_appointments
+FROM appointments
+GROUP BY appointment_type
+HAVING COUNT(*) >= 3;
+
+/*
+Output:
+appointment_type
+character varying
+total_appointments
+bigint
+1	Consultation	4
+2	Follow-Up	4
+*/
+
+-- 6 Using WHERE and HAVING together
+
+-- You can also use both where an having together,
+-- for further filtering.
+
+-- Show the appointment type, the total, and filter for completed, 
+-- then the total being more than 2
+
+SELECT
+    appointment_type,
+    COUNT(*) AS completed_appointments
+FROM appointments
+WHERE appointment_status = 'Completed'
+GROUP BY appointment_type
+HAVING COUNT(*) >= 2;
+
+/*
+Output:
+appointment_type
+character varying
+completed_appointments
+bigint
+1	Annual Exam	2
+2	Follow-Up	2
+*/
 
 -- Organizing Grouped Results
 -- ==================================
 
--- 8 Sorting Grouped Results
+-- 7 Sorting Grouped Results
 
--- 9 Limiting Grouped Results
+-- You can sort the grouped results by using the 
+-- ORDER BY, which after the aggregation and group by will sort
+
+-- Show the appointment type, then the total paid, 
+-- group by the appointment type, and sort by total paid
+
+SELECT
+    appointment_type,
+    ROUND(SUM(copay_amount), 2) AS total_cost
+FROM appointments
+GROUP BY appointment_type
+ORDER BY total_cost DESC;
+
+/*
+Output:
+appointment_type
+character varying
+total_cost
+numeric
+1	Follow-Up	105.00
+2	Annual Exam	60.00
+3	Consultation	50.00
+4	Diagnostic Test	50.00
+5	Sick Visit	15.00
+*/
 
--- 03 Conditional Aggregation/03_conditional_aggregation.sql
+++ 03 Conditional Aggregation/03_conditional_aggregation.sql
@@ -13,36 +13,283 @@ Goal: Learn how to create Conditional Aggregations in SQL
 
 -- 1 Count Matching Rows with SUM()
 
--- 2 Count Matching Rows with COUNT() 
+-- You can do condtional counting with SUM() and CASE(), 
+-- having the condition returning 1 else 0
+
+-- Show the total completed the appointments using CASE() and SUM()
+
+SELECT
+    SUM(
+        CASE
+            WHEN appointment_status = 'Completed' THEN 1
+            ELSE 0
+        END
+    ) AS completed_appointments
+FROM appointments;
+
+/*
+Output:
+completed_appointments
+bigint
+1	6
+*/
+
+-- 2 Count Matching Rows with COUNT()
+
+-- You can also use COUNT to do conditional counting,
+-- for the CASE() only include the condition.
+
+-- Show the total completed appointments using COUNT() and CASE() 
+
+SELECT
+    COUNT(
+        CASE
+            WHEN appointment_status = 'Completed' THEN 1
+        END
+    ) AS total_completed_appointments
+FROM appointments;
+
+/*
+Output:
+total_completed_appointments
+bigint
+1	6
+*/
 
 -- 3 Create Multiple Conditional Counts
+
+-- You can stack the counts to have mutltiple different counts
+-- for differing conditions
+
+-- Show the total for completed, cancelled, and then scheduled
+
+SELECT
+    SUM(
+        CASE
+            WHEN appointment_status = 'Completed' THEN 1
+            ELSE 0
+        END
+    ) AS completed_appointments,
+
+    SUM(
+        CASE
+            WHEN appointment_status = 'Cancelled' THEN 1
+            ELSE 0
+        END
+    ) AS cancelled_appointments,
+
+    SUM(
+        CASE
+            WHEN appointment_status = 'Scheduled' THEN 1
+            ELSE 0
+        END
+    ) AS scheduled_appointments
+FROM appointments;
+
+/*
+Output:
+completed_appointments
+bigint
+cancelled_appointments
+bigint
+scheduled_appointments
+bigint
+1	6	2	2
+*/
 
 -- Conditional Total
 -- =========================================
 
 -- 4 Sum Values that Meet a Condition
 
+-- You can find the total for the sums by using the SUM() and 
+-- CASE() by chaning the condtion outcome to a numeric column
+
+-- Show the total revenue that was created from the completed appointments
+
+SELECT
+    SUM(
+        CASE
+            WHEN appointment_status = 'Completed'
+                THEN copay_amount
+            ELSE 0
+        END
+    ) AS completed_revenue
+FROM appointments;
+
+/*
+Output:
+completed_revenue
+numeric
+1	175.00
+*/
+
 -- Conditional Averages
 -- =========================================
 
 -- 5 Average Values That Meet a Condition
+
+-- You can find the averages of the values from a numeric value column
+-- using a condition. 
+
+-- Find the average cost for the completed appointments
+
+SELECT
+    ROUND(
+        AVG(
+            CASE
+                WHEN appointment_status = 'Completed'
+                    THEN copay_amount
+                ELSE NULL
+            END
+        ),
+        2
+    ) AS average_completed_cost
+FROM appointments;
+
+/*
+Output:
+average_completed_cost
+numeric
+1	29.17
+*/
 
 -- Conditional Minimum and Maximum
 -- =========================================
 
 -- 6 Find the conditional Minimum and Maximum Values
 
+-- You can also find the Min and the Max by using the same
+-- MIN() or MAX() and then the CASE()
+
+-- Show the max payment and the lowest payment
+-- for the completed appointments
+
+SELECT
+    MIN(
+        CASE
+            WHEN appointment_status = 'Completed'
+                THEN copay_amount
+        END
+    ) AS lowest_completed_cost,
+
+    MAX(
+        CASE
+            WHEN appointment_status = 'Completed'
+                THEN copay_amount
+        END
+    ) AS highest_completed_cost
+FROM appointments;
+
+/*
+Output:
+lowest_completed_cost
+numeric
+highest_completed_cost
+numeric
+1	15.00	50.00
+*/
+
 -- Conditional Aggregation with GROUP BY
 -- =========================================
 
 -- 7 Calculate Conditional Results for Each GROUP
+
+-- You can also group the conditional aggregation by a category column
+
+-- Show the appointment type, then the total appointments,
+-- Count the completed appointments, group by appointment type
+
+SELECT
+    appointment_type,
+
+    COUNT(*) AS total_appointments,
+
+    SUM(
+        CASE
+            WHEN appointment_status = 'Completed' THEN 1
+            ELSE 0
+        END
+    ) AS completed_appointments,
+
+    SUM(
+        CASE
+            WHEN appointment_status = 'Cancelled' THEN 1
+            ELSE 0
+        END
+    ) AS cancelled_appointments
+
+FROM appointments
+GROUP BY appointment_type;
+
+/*
+Output:
+appointment_type
+character varying
+total_appointments
+bigint
+completed_appointments
+bigint
+cancelled_appointments
+bigint
+1	Sick Visit	1	1	0
+2	Consultation	4	0	2
+3	Annual Exam	2	2	0
+4	Diagnostic Test	1	1	0
+5	Follow-Up	4	2	0
+*/
 
 -- Conditional Rates and Percentages
 -- =========================================
 
 -- 8 Calculate a Conditional Percentage
 
+-- You can calculate the conditional percentages by dividing the conditional count by 
+-- the total count
+
+SELECT
+    ROUND(
+        100.0
+        * SUM(
+            CASE
+                WHEN appointment_status = 'Completed' THEN 1
+                ELSE 0
+            END
+        )
+        / COUNT(*),
+        2
+    ) AS completion_percentage
+FROM appointments;
+
+/*
+Output:
+completion_percentage
+numeric
+1	50.00
+*/
+
 -- Multiple Conditions
 -- =========================================
 
 -- 9 Combine Conditions with AND
+
+-- Inside the CASE you can use the AND, OR, IN, and BETWEEN for 
+-- more filtering
+
+SELECT
+    SUM(
+        CASE
+            WHEN appointment_status = 'Completed'
+             AND copay_amount >= 50
+                THEN 1
+            ELSE 0
+        END
+    ) AS completed_high_cost_appointments
+FROM appointments;
+
+/*
+Output:
+completed_high_cost_appointments
+bigint
+1	1
+*/
